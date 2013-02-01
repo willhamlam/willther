618e0604432c7fd8ae9a10336cd65475e49f79b1
William Lam <iamhandian@gmail.com>
2013-02-01 22:11:46 +0800

    finish basic function

--- willther.js
+++ willther.js
@@ -1,14 +1,28 @@
 (function(){
 	$(function(){
 
-		$('a').on('click',function(){
-			$('body').toggleClass('color');
-			$('.area-time').toggleClass('font-color');
-			// console.log('success');
-		})
+		// $('a').on('click',function(){
+		// 	$('body').toggleClass('color');
+		// 	$('.area-time').toggleClass('font-color');
+		// 	// console.log('success');
+		// })
 
 		var APPID = '1xLcLN3c'; // Your Yahoo Application ID
 		var DEG = 'c';  // c for celsius, f for fahrenheit
+		var weatherNUM;
+
+		var weatherIconMap = [
+		'storm', 'storm', 'storm', 'lightning', 'lightning', 'snow', 'hail', 'hail',
+		'drizzle', 'drizzle', 'rain', 'rain', 'rain', 'snow', 'snow', 'snow', 'snow',
+		'hail', 'hail', 'fog', 'fog', 'fog', 'fog', 'wind', 'wind', 'snowflake',
+		'cloud', 'cloud_moon', 'cloud_sun', 'cloud_moon', 'cloud_sun', 'moon', 'sun',
+		'moon', 'sun', 'hail', 'sun', 'lightning', 'lightning', 'lightning', 'rain',
+		'snowflake', 'snowflake', 'snowflake', 'cloud', 'rain', 'snow', 'lightning'
+		];
+
+		var weatherColorMap = [
+		'0','','','','','','','','','','','','','','','','','','','','','','','','','','#419EB3','','','','','','','#333333','','','','','','','','','','','','','',''
+		];
 
 		//Does this browser support geolocation?
 		if (navigator.geolocation) {
@@ -29,7 +43,9 @@
 					weatherYQL = 'http://query.yahooapis.com/v1/public/yql?q='+encodeURIComponent(wsql)+'&format=json&callback=?',
 					code, city, results, woeid;
 
-			
+			$('body').on('click', 'a', function(e){
+				e.preventDefault();
+			})
 			
 			$.getJSON(geoAPI,function(r){
 
@@ -45,8 +61,27 @@
 
 						if(r.query.count == 1){
 
-							//create the weather items in the #scroller UL
+							//create the weather items in the #scroller UL\
+							console.log(r);
+							var item = r.query.results.channel.item.condition,
+									loc = r.query.results.channel.location.city;
+							addWeather(item.code,"now",item.temp,loc,0);
+							// weatherColor(item.code,0);
 
+							for(var i=0;i<2;i++){
+								item = r.query.results.channel.item.forecast[i];
+								addWeather(item.code,item.day,item.high,loc,i+1);
+								// weatherColor(item.code,i+1);
+							}
+							window.slider = new Swipe(document.getElementById('slider'));
+							var index = $('.day'+slider.getPos() ).attr('data-index');
+							var changeColor = weatherColorMap[index];
+							$('body').css({
+								'background-color': changeColor
+							})
+							$('.area-time').css({
+								'color': changeColor
+							})
 						}
 
 					})
@@ -76,6 +111,49 @@
 			console.log(msg);
 		}
 
-		
+		function addWeather(code,day,condition,loc,index){
+
+			var markup = '<li data-index="'+ code +'" style=\"display:block\" class=\"day'+ index +'\">' +
+						'<div class="slider-wrap">' +
+							'<div class="slider-container">'+
+								'<div class="col left">'+
+									'<img class="woeid-icon" src=\"img/' + weatherIconMap[code] + '.png\" />'+
+									'<h2 class="woeid-area">' + loc +'</h2>'+
+								'</div>'+
+								'<div class="col right">'+
+									'<h1 class="area-tem">' + condition + '&deg;</h1>'+
+									'<div class="area-time">' + day + '</div>'+
+								'</div>'+
+							'</div>'+
+						'</div>'+
+					'</li>'
+			console.log(code);
+			$('.weather-slider ul').append(markup);
+					
+		}
+
+		$('.last-day').on('click',function(){
+			slider.prev();
+			var index = $('.day'+slider.getPos() ).attr('data-index');
+			var changeColor = weatherColorMap[index];
+			$('body').css({
+				'background-color': changeColor
+			})
+			$('.area-time').css({
+				'color': changeColor
+			})
+		});
+
+		$('.next-day').on('click',function(){
+			slider.next();
+			var index = $('.day'+slider.getPos() ).attr('data-index');
+			var changeColor = weatherColorMap[index];
+			$('body').css({
+				'background-color': changeColor
+			})
+			$('.area-time').css({
+				'color': changeColor
+			})
+		});
 	});
 })(jQuery);
